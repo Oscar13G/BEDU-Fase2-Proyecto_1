a92a75e2a56ed3d7cfd3b37428ef078c578d2094
Emanuel Alamilla <e.alamilla318@gmail.com>
2022-09-02 11:57:04 -0500

    some changes

--- js/components/components.js
+++ js/components/components.js
@@ -1,5 +1,7 @@
 function buildRecipeCard(data) {
+  console.log(data);
   const card = document.createElement("div");
+  const nombreReceta = data.strMeal;
   card.classList.add(
     "col-12",
     "col-md-4",
@@ -10,29 +12,20 @@ function buildRecipeCard(data) {
   );
   card.innerHTML =
     '<div class="card card-meal" style="width: 18rem">' +
-    '<img src="img/pizza.jpg" class="card-img-top" alt="..." />' +
+    '<img src="' +
+    data.strMealThumb +
+    '" class="card-img-top" alt="..." />' +
     '<div class="card-body">' +
-    '<h5 class="card-title">Pizza Planeta</h5>' +
+    '<h5 class="card-title">' +
+    nombreReceta.substring(0, 18) +
+    "</h5>" +
     '<div class="card-btn">' +
-    '<a href="#" class="btn">Ver receta</a>' +
+    '<a idMeal="' +
+    data.idMeal +
+    '" class="btn btnMealDetails">Ver receta</a>' +
     "</div>" +
     "</div>" +
     "</div>";
   return card;
 }
 export { buildRecipeCard };
-{
-  /* <div
-          class="col-12 col-md-4 mx-auto d-flex justify-content-center card-container"
-        >
-          <div class="card card-meal" style="width: 18rem">
-            <img src="img/pizza.jpg" class="card-img-top" alt="..." />
-            <div class="card-body">
-              <h5 class="card-title">Pizza Planeta</h5>
-              <div class="card-btn">
-                <a href="#" class="btn">Ver receta</a>
-              </div>
-            </div>
-          </div>
-        </div> */
-}
--- js/index.js
+++ js/index.js
@@ -13,15 +13,31 @@ function getMealMach(value) {
     ).then((response) => response.json());
   }
 }
-
+// Funcion API Meal Details
+function getMealDetails(id_meal) {
+  return fetch(
+    "https://www.themealdb.com/api/json/v1/1/lookup.php?i=" + id_meal
+  ).then((response) => response.json());
+}
+function cleanRecipes() {
+  document.getElementById("recipeContainer").innerHTML = "";
+}
 document.querySelector("#randomBtn").addEventListener("click", function (e) {
-  const pito = buildRecipeCard("alaska");
-  document.getElementById("recipeContainer").append(pito);
-
+  cleanRecipes();
   getMealRandom().then(function (data) {
-    console.log(data);
-    const card = buildRecipeCard(data);
+    const card = buildRecipeCard(data.meals[0]);
+    card.addEventListener("click", function (e) {
+      setModalData(e);
+    });
     document.getElementById("recipeContainer").append(card);
-    titleList.textContent = "Recipe Random";
   });
+  e.preventDefault();
 });
+
+function setModalData(e) {
+  const id_meal = e.target.getAttribute("idMeal");
+  getMealDetails(id_meal).then(function (data) {
+    console.log(data);
+  });
+  console.log(id_meal);
+}
